52b4dc87b9c5f19766d670e5cf1ffdd68fc8daf1
iTitou <moiandme@gmail.com>
2015-02-26 02:16:24 +0100

    [ref] Remove old files

--- git-config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git config --global color.ui true
-gitalias='git config --global alias'
-$gitalias.co checkout
-$gitalias.ci commit
-$gitalias.st status
-$gitalias.br branch
-$gitalias.hist "log --all --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-$gitalias.up "remote update --prune"
