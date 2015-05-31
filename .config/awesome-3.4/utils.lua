1273d75992df4a92204d458bf7caf1a87b14d402
iTitou <moiandme@gmail.com>
2015-05-31 03:09:35 +0200

    [fix] Missing file

---
+++ .config/awesome-3.4/utils.lua
@@ -0,0 +1,29 @@
+function cpu_percent()
+  local last_busy, last_idle = 0, 0
+
+  local res = function ()
+    for line in io.lines("/proc/stat") do
+      local user, nice, system, idle, iowait, irq, softirq = string.match(line, "cpu +(%d+) +(%d+) +(%d+) +(%d+) +(%d+) +(%d+) +(%d+)")
+      if user then
+        local busy = user + nice + system + iowait + irq + softirq
+        local dbusy = busy - last_busy
+        local didle = idle - last_idle
+        last_busy = busy
+        last_idle = idle
+        return math.floor(0.5 + 100 * dbusy / (dbusy + didle))
+      end
+    end
+  end
+
+  res()
+  return res
+end
+
+function load_avg()
+  for line in io.lines("/proc/loadavg") do
+    local match = string.match(line, "(%d+.%d+ +%d+.%d+ +%d+.%d+)")
+    if match then
+      return match
+    end
+  end
+end
