bb1c6aa0c34f08c3443df79b3e68f4a6872003ca
iTitou <moiandme@gmail.com>
2017-03-20 12:13:05 +0100

    [enh] Mo4r fish aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,25 +1,30 @@
-set -U fish_user_paths ~/bin
-set -U fish_user_paths ~/.local/bin
-set -U fish_user_paths ~/.cabal/bin
+# Common PATHs to add
+for p in "~/bin" "~/.cabal/bin" "~/.local/bin"
+    if test -d "$p"
+        echo add $p to PATH
+        set -U fish_user_paths $p
+    end
+end
+
 
 # Prompt
 set theme_display_git_untracked no
 . $functions/fish_prompt.fish
 
+
 # Aliases
 alias urlab-ssh "ssh -t azog.urlab.be ssh"
+alias legacy-ssh "ssh -oKexAlgorithms=+diffie-hellman-group1-sha1"
 alias ls "ls --color"
 alias ll "ls -l"
 alias la "ll -A"
 alias lll "tree -C"
+alias less "less -R"
+alias lock "xscreensaver-command --lock"
 
 # grc (colorize command output)
 alias ps "grc ps"
 alias mount "grc mount"
 alias netstat "grc netstat"
-
-function cpu-hard-limit
-    for i in (seq 0 7)
-        sudo cpufreq-set -c $i -u 800000
-    end
-end
+alias df "grc df"
+alias du "grc du"
